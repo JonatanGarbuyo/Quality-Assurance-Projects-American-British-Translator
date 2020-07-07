5d97ad5934db53a92cba0cc68f6a77cb0e214fff
Glitch (decorous-leather-light) <none>
2020-07-07 14:09:54 +0000

    🍇💖 Checkpoint ./public/translator.js:17402204/11

--- public/translator.js
+++ public/translator.js
@@ -3,6 +3,17 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+
+
+
+
+
+
+
+
+
+
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
