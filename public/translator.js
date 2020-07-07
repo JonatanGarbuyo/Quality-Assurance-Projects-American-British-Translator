4846df2e11196ac0ae523fb1ceb9976964240b4f
Glitch (decorous-leather-light) <none>
2020-07-07 17:42:16 +0000

    🚶🐀 Checkpoint ./public/translator.js:17402204/2390

--- public/translator.js
+++ public/translator.js
@@ -3,16 +3,30 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+const textArea = document.getElementById("text-input");
+const localeSelect = document.getElementById("locale-select");
+const translateButton = document.getElementById("translate-btn");
+const clearButton = document.getElementById("clear-btn");
+const translatedSentence = document.getElementById("translated-sentence");
+const errorMessage = document.getElementById("error-msg");
+       
+const ClearText = () => {
+  textArea.value = ""
+  translatedSentence.value = ""
+}
+
+const Translate = () => {
+  translatedSentence.value = textArea.value;
+}
 
 
 
 
 
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", event => {
+  
+  clearButton.addEventListener("click", ClearText);
+});
 
 /* 
   Export your functions for testing in Node.
